d15ff6156868910220df872337af34f60d77f495
SAKAMOTO_Yasuaki <yasunoaddress.s@gmail.com>
2020-03-22 14:45:25 +0900

    msg

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ http.interceptors.request.use((config) => {
     return Promise.reject(error)
 })
 const App: React.FC = () => {
-    const [body, setPlantBody] = useState({});
+    const [body, setPlantBody] = useState([{ text: 'Learn Hooks' }]);
     const [value, setValue] = useState<string>('')
     const [isLogin, setIsLogin] = useState<boolean>(false)
 
@@ -58,13 +58,13 @@ const App: React.FC = () => {
             .then(response => {
                 console.log('status:', response.status); // 200
                 console.log('body:', response.data);     // response body.
-                setPlantBody({"a":"b"});
+                setPlantBody([{text:"string"}]);
 
 
                 // catchでエラー時の挙動を定義する
             }).catch(err => {
             console.log('err:', err);
-            setPlantBody({});
+            setPlantBody([]);
         });
     }
 
