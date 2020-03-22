068f474eb22e4c4c60e94bf1e71d26bd5cc3a96c
SAKAMOTO_Yasuaki <yasunoaddress.s@gmail.com>
2020-03-22 11:32:44 +0900

    msg

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,19 @@ import axios from 'axios';
 
 
 const liff = window.liff
-axios.defaults.baseURL = 'https://mighty-anchorage-01609.herokuapp.com';
-axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
+const http = axios.create({
+  baseURL: `https://mighty-anchorage-01609.herokuapp.com/api/`
+})
+http.interceptors.request.use((config) => {
+  if (token) {  
+    config.headers.Authorization = `Bearer HkYXti6w52aaG9XcI0ziDxhodbqkw3/hkZ8heSNcGVceJsh6iwldnuAYr7X363ErEMUBVKvzrRHJcFnS/MwjFg==`
+    return config
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
 const App: React.FC = () => {
     const [value, setValue] = useState<string>('')
         const [isLogin, setIsLogin] = useState<boolean>(false)
@@ -53,6 +62,11 @@ data: {},
                 }
                 })
     }
+    const getPlant = () =>{
+        response = http.get("/plant_categories");
+        console.log(reponse);
+        this.setState({ events: response.data });
+    }
 
     return (
             <div className="App">
@@ -65,6 +79,11 @@ data: {},
             <div className="qrButton">
             <Button variant="contained" color="secondary" onClick={() => openQR()} disabled={!isLogin}>
             Open QR camera
+            </Button>
+            </div>
+            <div className="qrButton">
+            <Button variant="contained" color="secondary" onClick={() => getPlant()} disabled={!isLogin}>
+            Get Plant
             </Button>
             </div>
             </div>
